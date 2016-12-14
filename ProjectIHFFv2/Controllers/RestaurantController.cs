8415c4f1901d8a6cdd027c655d469402df569ca3
MathijsvanHeerden <567604@student.inholland.nl>
2016-12-14 16:03:32 +0100

    Restaurant overzicht

---
+++ ProjectIHFFv2/Controllers/RestaurantController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectIHFFv2.Models; 
+
+namespace ProjectIHFFv2.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantRepository resrep = new RestaurantRepository(); 
+        // GET: Restaurant
+        public ActionResult Bloemendaal()
+        {
+            IEnumerable<Event> resb = resrep.GetRestaurantsBloemendaal(); 
+
+            return View(resb);
+        }
+    }
+}
---
+++ ProjectIHFFv2/Models/Repositories/RestaurantRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIHFFv2.Models
+{
+    
+
+    public class RestaurantRepository
+    {
+        private iHFF1617S_A3Entities1 ctx = new iHFF1617S_A3Entities1();
+
+
+        public IEnumerable<Event> GetRestaurantsBloemendaal()
+        {
+            DateTime dag = new DateTime(2017, 1, 11);
+            IEnumerable<Event> res = ctx.Event.Where(r => r.type == 2 && r.Locatie.plaats == "Bloemendaal");
+
+            IEnumerable<Event> resU = res.DistinctBy(e => e.naam); 
+           // IEnumerable<Event> resBl = ctx.Event.DistinctBy(r => r.naam) ; 
+              
+            return resU; 
+             
+        } 
+    }
+}
