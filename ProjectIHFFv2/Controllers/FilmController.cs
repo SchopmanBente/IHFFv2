786aff94fa9a83800fbcfa0fba188b579f72e7b7
SchopmanBente <b.l.schopman@gmail.com>
2016-12-14 13:47:08 +0100

    alles toegevoegd

---
+++ ProjectIHFFv2/Controllers/FilmController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectIHFFv2.Models;
+
+namespace ProjectIHFFv2.Controllers
+{
+    public class FilmController : Controller
+    {
+        //
+        // GET: /Film/
+        private FilmRepository filmrepository = new FilmRepository();
+
+        public ActionResult Woensdag()
+        {
+            IEnumerable<Film> films = filmrepository.GetAll();
+            return View(films);
+        }
+	}
+}
---
+++ ProjectIHFFv2/Models/Repositories/FilmRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectIHFFv2.Models
+{
+    public class FilmRepository
+    {
+        private iHFF1617S_A3Entities1 ctx = new iHFF1617S_A3Entities1();
+
+        public IEnumerable<Film> GetAll()
+        {
+            return ctx.Film;
+        }
+    }
+}
