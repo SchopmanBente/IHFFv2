ccb0b2c25e42aea3af0ae4e8ee4e144704b65d03
ivotjebot <game4life49@live.nl>
2016-12-16 14:29:03 +0100

    Nog meer veranderingen

--- ProjectIHFFv2/Models/WishlistModel.cs
+++ ProjectIHFFv2/Models/WishlistModel.cs
@@ -22,21 +22,9 @@ namespace ProjectIHFFv2.Models
         {
             List<WishlistItem> wishlist = new List<WishlistItem>();
             DateTime standaardTijd = new DateTime(2017, 1, 11, 11, 00, 00);
-            int i = 0;
-            //Geef een datetime waarde aan de tijd die bovenaan de tabel staat. Hier kan je mee vergelijken. Elke keer wanneer je nu naar een nieuwe rij gaat moet je de datum wijzigen!
-            while (i < 24)
-            {
-                WishlistItem bla = new WishlistItem();
-                if (i % 2 == 0)
-                {
-                    bla.EerstMogelijkeTijd = EerstMogelijkeTijd.AddHours(i);
-                }
-                else
-                {
-                    bla.EerstMogelijkeTijd = EerstMogelijkeTijd.AddHours(i).AddMinutes(30);
-                    i++;
-                }
-            }
+            //MAAK EEN ARRAY MET COORDINATEN EN NEEM VAN DE COORDINAAT DE EERSTE WAARDE BOVENAAN EN DE EERSTE WAARDE LINKS. MAAK HIER EEN DATETIME VAN.
+            int[,] tijdNavigatieArray = new int[20,20];
+
             //Van alle tijden die er zijn is er nu een tijd ingevuld. Bij de tabelcreatie wordt een if (!null) gebruikt.
 
             return wishlist;
