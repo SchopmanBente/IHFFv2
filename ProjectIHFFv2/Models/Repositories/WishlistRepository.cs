d8dfb01c4b4c1fe17b8cae097080147f99f948e5
ivotjebot <game4life49@live.nl>
2016-12-16 12:16:40 +0100

    Wishlist verder gemaakt

---
+++ ProjectIHFFv2/Models/Repositories/WishlistRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectIHFFv2.Models.Repositories
+{
+    public class WishlistRepository
+    {
+        //Hier session of cookies pakken wanneer de 'database' aangeroepen wordt.
+        public List<WishlistModel> GetWishlist()
+        {
+            List<WishlistModel> wishlist = new List<WishlistModel>();
+            //pak uit de session of cookie alle dingen die aan die hieraan zijn toegevoegd.
+            return wishlist;
+        }
+
+        public void AddToWishlist(WishlistModel item)
+        {
+            //voeg item toe aan session die een lijst van wishlistmodels bevat.
+        }
+
+        public void RemoveFromWishlist(WishlistModel item)
+        {
+            //verwijder de wishlistmodel uit de wishlistlijst.
+        }
+
+        public void EditWishtlist(WishlistModel item)
+        {
+
+        }
+
+    }
+}
---
+++ ProjectIHFFv2/Models/WishlistModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectIHFFv2.Models
+{
+    public class WishlistModel
+    {
+        public int EventId { get; set; }
+        public string titel { get; set; }
+        public DateTime beginTijd { get; set; }
+        public DateTime eindTijd { get; set; }
+        public int prijs { get; set; }
+        public int locatieId { get; set; }
+        public int type { get; set; }
+        public int aantal { get; set; }
+        public DateTime EerstMogelijkeTijd { get; set; }
+
+
+        public List<WishlistModel> MakeWishlist()
+        {
+            List<WishlistModel> wishlist = new List<WishlistModel>();
+            DateTime standaardTijd = new DateTime(2017, 1, 11, 11, 00, 00);
+            int i = 0;
+            //Geef een datetime waarde aan de tijd die bovenaan de tabel staat. Hier kan je mee vergelijken. Elke keer wanneer je nu naar een nieuwe rij gaat moet je de datum wijzigen!
+            while(i < 24){
+                WishlistModel bla = new WishlistModel();
+                if (i % 2 == 0) {
+                    bla.EerstMogelijkeTijd = EerstMogelijkeTijd.AddHours(i);
+                } else {
+                    bla.EerstMogelijkeTijd = EerstMogelijkeTijd.AddHours(i).AddMinutes(30);
+                    i++;
+                }
+            }
+            return wishlist;
+        }
+    }
+}
