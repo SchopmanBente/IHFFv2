4584855a9306fede5d81edca0b8c29884e347f04
ivotjebot <game4life49@live.nl>
2016-12-16 10:39:41 +0100

    programma samenstellen aangemaakt

---
+++ ProjectIHFFv2/Controllers/WishlistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectIHFFv2.Models;
+
+namespace ProjectIHFFv2.Controllers
+{
+    public class WishlistController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
