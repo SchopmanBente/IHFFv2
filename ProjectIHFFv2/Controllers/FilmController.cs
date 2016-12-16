53987b6297b35dd626cb1a06a889f6ce0848c292
SchopmanBente <b.l.schopman@gmail.com>
2016-12-16 17:15:57 +0100

    Aanpassingen selecties + vertalingen links van NL naar EN

--- ProjectIHFFv2/Controllers/FilmController.cs
+++ ProjectIHFFv2/Controllers/FilmController.cs
@@ -15,30 +15,34 @@ namespace ProjectIHFFv2.Controllers
         private PresentationViews presentation = new PresentationViews();
 
         public ActionResult Woensdag()
+        public ActionResult Wednesday()
         {
             IEnumerable<FilmOverviewPresentationModel> films = presentation.GetAllFilmsForDay(new DateTime(2017, 1, 11, 00, 00, 00));
             return View(films);
         }
 
         public ActionResult Donderdag()
+        public ActionResult Thursday()
         {
             IEnumerable<FilmOverviewPresentationModel> films = presentation.GetAllFilmsForDay(new DateTime(2017, 1, 12, 00, 00, 00));
             return View(films);
         }
 
         public ActionResult Vrijdag()
+        public ActionResult Friday()
         {
             IEnumerable<FilmOverviewPresentationModel> films = presentation.GetAllFilmsForDay(new DateTime(2017, 1, 13, 00, 00, 00));
             return View(films);
         }
 
         public ActionResult Zaterdag()
+        public ActionResult Saturday()
         {
             IEnumerable<FilmOverviewPresentationModel> films = presentation.GetAllFilmsForDay(new DateTime(2017, 1, 14, 00, 00, 00));
             return View(films);
         }
 
-        public ActionResult Zondag()
+        public ActionResult Sunday()
         {
             IEnumerable<FilmOverviewPresentationModel> films = presentation.GetAllFilmsForDay(new DateTime(2017, 1, 15, 00, 00, 00));
             return View(films);
