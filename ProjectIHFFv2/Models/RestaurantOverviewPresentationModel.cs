947d3696269e00712e2f8e23d74b622fc7a3d483
MathijsvanHeerden <567604@student.inholland.nl>
2016-12-20 13:19:21 +0100

    De homepagina opgefokt en Restaurant overview css deels toegepast

---
+++ ProjectIHFFv2/Models/RestaurantOverviewPresentationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIHFFv2.Models
+{
+   public class RestaurantOverviewPresentationModel
+    {
+        public Event Event { get;set; }
+
+        public RestaurantOverviewPresentationModel(Event Event)
+        {
+            this.Event = Event; 
+        }
+    }
+}
