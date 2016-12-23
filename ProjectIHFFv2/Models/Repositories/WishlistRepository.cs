168b78902a601300886ae9bfccffa0bdba18fa9a
ivotjebot <game4life49@live.nl>
2016-12-23 11:06:12 +0100

    wishlist tijd die al in beslag is toegevoegd

--- ProjectIHFFv2/Models/Repositories/WishlistRepository.cs
+++ ProjectIHFFv2/Models/Repositories/WishlistRepository.cs
@@ -10,13 +10,69 @@ namespace ProjectIHFFv2.Models
     public class WishlistRepository : Controller //:controller zodat de session gebruikt kan worden.
     {
 
-        public List<WishlistItem> MakeWishlist( List<WishlistItem> meegekregenWishlist)
+        public void AddToWishlist(Event item, int aantal, List<WishlistItem> items)
+        {
+            int aantalFalse = 0; //houdt bij hoeveel keer er false is teruggegeven op de vraag: Is de datum en tijd al in gebruik?
+            foreach (WishlistItem bestaandItem in items)
+            {
+                if (bestaandItem.beginTijd.Day == item.begin_datumtijd.Day) //deze if heeft geen else, ga als false gewoon naar de volgende bestaandItem
+                {
+                    if ((item.eind_datumtijd <= bestaandItem.beginTijd && item.eind_datumtijd > bestaandItem.beginTijd) || (item.eind_datumtijd <= bestaandItem.beginTijd && bestaandItem.eindTijd < item.begin_datumtijd)) //check of de tijd al in gebruik is, als dat zo is wordt er false ge returnt.
+                    {
+                        aantalFalse++;
+                    }
+                }
+            }
+
+            WishlistItem wishlistItem = new WishlistItem();
+            List<WishlistItem> wishlist = items;
+
+            if (aantalFalse == 0) {
+                wishlistItem.aantal = aantal;
+                wishlistItem.beginTijd = item.begin_datumtijd;
+                wishlistItem.eindTijd = item.eind_datumtijd;
+                wishlistItem.EventId = item.EventId;
+                wishlistItem.locatieId = item.locatie_id;
+                wishlistItem.naam = item.naam;
+                wishlistItem.prijs = item.prijs * aantal;
+                wishlistItem.type = item.type;
+            }
+            else
+            {
+                //zeg dat het toevoegen niet kan...
+            }
+            if (!wishlist.Exists(w => w.EventId == item.EventId))
+            {
+                wishlist.Add(wishlistItem);
+            }
+        }
+
+        public void RemoveFromWishlist(WishlistItem item)
+        {
+            List<WishlistItem> wishlist = Session["wishlist"] as List<WishlistItem>;
+            foreach (WishlistItem wishlistItem in wishlist)
+            {
+                if (wishlistItem.EventId == item.EventId)
+                {
+                    wishlist.Remove(wishlistItem);
+                }
+            }
+        }
+
+        public void EditWishtlist(WishlistItem item)
+        {
+
+        }
+
+
+        public List<WishlistItem> MakeWishlist(List<WishlistItem> meegekregenWishlist)
         {
             //HIER OOK NOG CHECKEN OF BEPAALDE ITEMS AL BESTAAN! HIER EEN LIJST MEEGEVEN IS HIER HET NU VAN.
             List<WishlistItem> wishlist = meegekregenWishlist;
-                string [,] tijdNavigatieArray = MakeTimeArray();
+            string[,] tijdNavigatieArray = MakeTimeArray();
 
-            foreach (WishlistItem item in wishlist) {
+            foreach (WishlistItem item in wishlist)
+            {
                 int x = 0;
                 int y = 0;
                 string heleDatum = item.beginTijd.ToString("dd'/'MM'/'yyyy HH:mm");
@@ -46,7 +102,7 @@ namespace ProjectIHFFv2.Models
                     if ((item.yCoordinaat != 0) && (item.xCoordinaat != 0))
                     {
                         string tijdsduur = (item.eindTijd - item.beginTijd).ToString(); //krijg het verschil in tijd te zien.
-                        int uren = Int32.Parse(tijdsduur.Substring(0,2));
+                        int uren = Int32.Parse(tijdsduur.Substring(0, 2));
                         int minuten = Int32.Parse(tijdsduur.Substring(3, 2));
                         int totaalminuten = (uren * 60) + minuten;
                         item.colspan = totaalminuten / 30;
@@ -61,93 +117,47 @@ namespace ProjectIHFFv2.Models
 
         private string[,] MakeTimeArray() //maak array 1x
         {
-            
-                 string[,] gemaakteArray = new string[30,6];
-                int x = 0;
-                int y = 0;
-                string tijd = "11:00";
-                int uur = 11;
-                bool doorgaan = true;
-                
-                while (doorgaan)
-                {
-                    if (tijd != "24:30") //voeg alle tijden toe aan rij 0 van de array.
-                    {
-                        x++;
-                        if (tijd.Contains("30"))
-                        {
-                            gemaakteArray[x, y] = tijd;
-                            uur++;
-                            tijd = uur.ToString() + ":00";
-                        }
-                        else
-                        {
-                            if (tijd == "24:00")
-                            {
-                                gemaakteArray[x, y] = "00:00";
-                            }
-                            gemaakteArray[x, y] = tijd;                           
-                            tijd = uur.ToString() + ":30";
-                            
-                        }
-                    } //alle tijden zijn toegevoegd, nu de dagen nog.
+            string[,] gemaakteArray = new string[30, 6];
+            int x = 0;
+            int y = 0;
+            string tijd = "11:00";
+            int uur = 11;
+            bool doorgaan = true;
 
-                    if (x == 27)
+            while (doorgaan)
+            {
+                if (tijd != "24:30") //voeg alle tijden toe aan rij 0 van de array.
+                {
+                    x++;
+                    if (tijd.Contains("30"))
                     {
-                        gemaakteArray[0, 1] = "11/01/2017";
-                        gemaakteArray[0, 2] = "12/01/2017";
-                        gemaakteArray[0, 3] = "13/01/2017";
-                        gemaakteArray[0, 4] = "14/01/2017";
-                        gemaakteArray[0, 5] = "15/01/2017";
-                        doorgaan = false;
+                        gemaakteArray[x, y] = tijd;
+                        uur++;
+                        tijd = uur.ToString() + ":00";
                     }
-                }
-                return gemaakteArray;
-        }
+                    else
+                    {
+                        if (tijd == "24:00")
+                        {
+                            gemaakteArray[x, y] = "00:00";
+                        }
+                        gemaakteArray[x, y] = tijd;
+                        tijd = uur.ToString() + ":30";
 
-        public void AddToWishlist(Event item, int aantal, List<WishlistItem> items)
-        {
-            //foreach (WishlistItem bestaandItem in items)
-            //{
-            //    //eindtijd > begintijd
-            //    if ((item.begin_datumtijd <= bestaandItem.beginTijd) || (item.begin_datumtijd >= bestaandItem.beginTijd) && )
-            //    {
+                    }
+                } //alle tijden zijn toegevoegd, nu de dagen nog.
 
-            //    }
-            //}
-            //voeg item toe aan session die een lijst van wishlistmodels bevat.
-            WishlistItem wishlistItem = new WishlistItem();
-            wishlistItem.aantal = aantal;
-            wishlistItem.beginTijd = item.begin_datumtijd;
-            wishlistItem.eindTijd = item.eind_datumtijd;
-            wishlistItem.EventId = item.EventId;
-            wishlistItem.locatieId = item.locatie_id;
-            wishlistItem.naam = item.naam;
-            wishlistItem.prijs = item.prijs * aantal;
-            wishlistItem.type = item.type;
-            List<WishlistItem> wishlist = items;
-            //List<WishlistItem> wishlist = Session["wishlist"] as List<WishlistItem>;
-            if (!wishlist.Exists(w => w.EventId == item.EventId))
-            {
-                wishlist.Add(wishlistItem);
-            }
-        }
-
-        public void RemoveFromWishlist(WishlistItem item)
-        {
-            List<WishlistItem> wishlist = Session["wishlist"] as List<WishlistItem>;
-            foreach (WishlistItem wishlistItem in wishlist)
-            {
-                if (wishlistItem.EventId == item.EventId)
+                if (x == 27)
                 {
-                    wishlist.Remove(wishlistItem);
+                    gemaakteArray[0, 1] = "11/01/2017";
+                    gemaakteArray[0, 2] = "12/01/2017";
+                    gemaakteArray[0, 3] = "13/01/2017";
+                    gemaakteArray[0, 4] = "14/01/2017";
+                    gemaakteArray[0, 5] = "15/01/2017";
+                    doorgaan = false;
                 }
             }
-        }
-
-        public void EditWishtlist(WishlistItem item)
-        {
-
+            return gemaakteArray;
         }
 
 
